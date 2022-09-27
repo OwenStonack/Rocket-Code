ce5e8e9d0ed47466387ea2126cc88f4d3edaa012
Owen (Owen) Stonack <owen_stonack@m018926.aw.org>
2022-09-27 16:04:15 -0700

    did code

---
+++ Rocket Code.ino
@@ -0,0 +1,19 @@
+void setup( ){
+  Serial.begin(9600);
+  pinMode(10, OUTPUT);
+
+  //do countdown
+  for(int i = 10; i > 0; i--){
+      Serial.println(i);
+      delay(1000);
+  }
+
+  //fire the motor
+  digitalWrite(10, HIGH);
+  delay(5000);
+  digitalWrite(10, LOW);
+}
+
+void loop(){
+  
+}
